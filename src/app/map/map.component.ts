5a805cadd8e16a18259931334cefb60b38206ae7
Camilo1090 <camilo.ruiz.casanova@correounivalle.edu.co>
2018-04-27 17:03:58 -0500

    add loading feature

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,10 +1,10 @@
-import { AfterViewInit, Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
-import { TdLoadingService } from "@covalent/core";
+import {AfterViewInit, ChangeDetectorRef, Component, ElementRef, HostListener, OnInit, ViewChild} from '@angular/core';
+import { TdLoadingService } from '@covalent/core';
 import * as Datamap from 'datamaps';
 import * as d3 from 'd3';
 
-import { GeosportsService } from "../services/geosports.service";
-import { Observable } from "rxjs/Observable";
+import { GeosportsService } from '../services/geosports.service';
+import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/distinctUntilChanged';
 import 'rxjs/add/operator/debounceTime';
 
@@ -29,11 +29,15 @@ export class MapComponent implements OnInit, AfterViewInit {
   minAge: number;
   maxAge: number;
 
+  emitMinAge: (event: KeyboardEvent) => void;
+  emitMaxAge: (event: KeyboardEvent) => void;
+
   constructor(public geosportsService: GeosportsService,
-              public _loadingService: TdLoadingService) { }
+              public _loadingService: TdLoadingService,
+              private _changeDetectorRef: ChangeDetectorRef) {  }
 
   ngOnInit() {
-    //subscribe to receive changes, with 300ms debounce
+    // subscribe to receive age inputs changes, with 300ms debounce
     this.minAgeChanges(300).subscribe(value => {
       // this.minAge = Number(value);
       this.updateParams();
@@ -47,24 +51,29 @@ export class MapComponent implements OnInit, AfterViewInit {
   }
 
   ngAfterViewInit() {
-    // this.drawMap();
-    this.registerLoading();
-    // this.firstMapRender();
+    this.firstMapRender();
     this.updateMap();
+    this._changeDetectorRef.detectChanges();
   }
 
-  // first render of the map
-  // firstMapRender(): void {
-  //   this.geosportsService.count(this.params).subscribe(response => {
-  //     this.drawMap(response);
-  //     this.resolveLoading();
-  //   });
-  // }
+  // first render of the map (empty)
+  firstMapRender(): void {
+    const that = this;
+    this.map = new Datamap({
+      element: that.mapContainer.nativeElement,
+      projection: 'mercator',
+      responsive: true,
+      fills: {
+        defaultFill: '#F5F5F5'
+      },
+      data: {}
+    });
+  }
 
   // retrieves server data with params and updates map data
   updateMap(): void {
+    this.registerLoading();
     this.geosportsService.count(this.params).subscribe(response => {
-      // this.updateMapData(response);
       this.drawMap(response);
       this.resolveLoading();
     });
@@ -81,7 +90,7 @@ export class MapComponent implements OnInit, AfterViewInit {
       projection: 'mercator',
       responsive: true,
       fills: {
-        defaultFill: "#F5F5F5"
+        defaultFill: '#F5F5F5'
       },
       data: dataset,
       geographyConfig: {
@@ -105,26 +114,19 @@ export class MapComponent implements OnInit, AfterViewInit {
     });
   }
 
-  // updates the data used by the map
-  updateMapData(rawData: any[]): void {
-    const dataset = this.parseRawData(rawData);
-
-    this.map.updateChoropleth(dataset);
-  }
-
   // generates a data object from server retrieved data
   // that can be interpreted by the Datamaps library
   parseRawData(rawData: any[]): any {
     // Datamaps expect data in format:
     // { "USA": { "fillColor": "#42a844", count: 75},
     //   "FRA": { "fillColor": "#8dc386", count: 43 } }
-    let dataset = {};
+    const dataset = {};
 
     // Colorize every country based on "count"
     // Colors should be unique for every value.
     // Create palette(using min/max series-value)
-    let onlyValues = rawData.map((item) => {
-      return item.count
+    const onlyValues = rawData.map((item) => {
+      return item.count;
     });
     const minValue = Math.min.apply(null, onlyValues);
     const maxValue = Math.max.apply(null, onlyValues);
@@ -133,7 +135,7 @@ export class MapComponent implements OnInit, AfterViewInit {
     // color can be whatever you wish
     const paletteScale = d3.scaleLinear()
       .domain([minValue, maxValue])
-      .range(["#EFEFFF", "#02386F"]); // blue color
+      .range(['#EFEFFF', '#02386F']); // blue color
 
     // fill dataset in appropriate format
     rawData.forEach((item) => {
@@ -164,30 +166,28 @@ export class MapComponent implements OnInit, AfterViewInit {
       }
     }
     // min age param
-    if (this.minAge)
+    if (this.minAge) {
       this.params['age.gte'] = this.minAge;
+    }
     // max age param
-    if (this.maxAge)
+    if (this.maxAge) {
       this.params['age.lte'] = this.maxAge;
+    }
   }
 
   // function to handle the change of selected radio button of the sex button group
   onSexChanged(): void {
-    this.registerLoading();
     this.updateParams();
     this.updateMap();
   }
-
-  emitMinAge: (event: KeyboardEvent) => void;
-  emitMaxAge: (event: KeyboardEvent) => void;
 
   // returns an Observable that emits whenever emitInput() is called
   minAgeChanges(debounce: number): Observable<string> {
     return Observable.create(observer => {
       // setup emitInput() to forward values to the subscriber
-      this.emitMinAge = (event:KeyboardEvent) => {
-        observer.next((<HTMLInputElement>event.target).value)
-      }
+      this.emitMinAge = (event: KeyboardEvent) => {
+        observer.next((<HTMLInputElement>event.target).value);
+      };
     }).distinctUntilChanged().debounceTime(debounce);
   }
 
@@ -195,9 +195,9 @@ export class MapComponent implements OnInit, AfterViewInit {
   maxAgeChanges(debounce: number): Observable<string> {
     return Observable.create(observer => {
       // setup emitInput() to forward values to the subscriber
-      this.emitMaxAge = (event:KeyboardEvent) => {
-        observer.next((<HTMLInputElement>event.target).value)
-      }
+      this.emitMaxAge = (event: KeyboardEvent) => {
+        observer.next((<HTMLInputElement>event.target).value);
+      };
     }).distinctUntilChanged().debounceTime(debounce);
   }
 
